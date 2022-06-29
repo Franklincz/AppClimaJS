d7a5bfc5f9568d7dc40aaaac45b44200b4e48cd0
Franklin Cunya <cunyazetafranklin@gmail.com>
2022-06-28 22:39:26 -0500

    inicio de proyecto

---
+++ main.js
@@ -0,0 +1,31 @@
+//consumiendo api con fetch 
+
+
+const api ={
+    key:'c38d0643ee6d2b0a4319c16cdda5ce88',
+    url:'https://api.openweathermap.org/data/2.5/weather'
+}
+
+
+const form =document.getElementById('search-form');
+const searchbox=document.getElementById('searchbox');
+
+
+function onSubmit(){
+    
+}
+
+
+
+
+
+
+
+
+
+form.addEventListener('submit',onSubmit,true);
+
+
+
+
+

ee0cba73963d4973df11e8821b056dbeb8b8d22e
Franklin Cunya <cunyazetafranklin@gmail.com>
2022-09-30 22:35:03 -0500

    get data API

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-//consumiendo api con fetch 
 
+
+//consumiendo api con fetch 
 
 const api ={
     key:'c38d0643ee6d2b0a4319c16cdda5ce88',
@@ -7,23 +8,41 @@ const api ={
 }
 
 
-const form =document.getElementById('search-form');
-const searchbox=document.getElementById('searchbox');
+async function search(query){
+    try{
+        const response= await fetch(`${api.url}?q=${query}&appid=${api.key}&lang=es`)
+        const data= await response.json()
+        console.log(data);
+        console.log(typeof(data))
+        cargadatos(data)
+    }catch(err){
+        console.log(err)
+        alert('Hubo un error')
+    }
 
-
-function onSubmit(){
-    
 }
 
 
 
 
+function onSubmit(event){
+    event.preventDefault()
+    alert(searchbox.value)
+    search(searchbox.value)
 
 
+}
 
+function cargadatos(data){
+    city.innerHTML=data.name
+}
 
-
-form.addEventListener('submit',onSubmit,true);
+const searchform =document.getElementById('search-form');
+const searchbox=document.getElementById('searchbox');
+const city =document.getElementById('city')
+console.log(searchform)
+console.log(city)
+searchform.addEventListener('submit',onSubmit,true);
 
 
 
